2c67097080195c5abd092689b99b59d63dfc2741
Jigsaw <j1g5aw@foxmail.com>
2021-01-04 18:41:10 +0800

    Fix illegal dice command

--- src/plugins/roll_dice/calculator.py
+++ src/plugins/roll_dice/calculator.py
@@ -4,26 +4,32 @@ import re
 # 替换标准骰子及惩罚奖励骰(3d6, 4d6k3)
 def dice_caculator(dice_expression):
 
-    message=dice_expression+' = [ '
+    
 
     # 匹配标准掷骰
     match_result=re.search(r"([0-9]*)d([0-9]*)(k([0-9]*))?",dice_expression)
+    try:dice_num=int(match_result.group(1))
+    except:dice_num=1
+    try:dice_face=int(match_result.group(2))
+    except:dice_face=100
+    try:dice_pick=int(match_result.group(4))
+    except:dice_pick=dice_num
+    
+    
+    if not dice_num or dice_num>100:return '非法骰数'
+    if not dice_face or dice_face>1000:return '非法骰面'
+    if not dice_pick or dice_pick>dice_num:return '非法骰数'
 
-    dice_num=int(match_result.group(1))
-    dice_face=int(match_result.group(2))
-    dice_pick=int(match_result.group(4))
     dice_result_list=[]
-
     for i in range(dice_num):
         dice_result=random.randint(1,dice_face)
         dice_result_list.append(dice_result)
-    
-    if dice_pick:
-        dice_result_list.sort(reverse=True)
-        for i in range(len(dice_result_list)-dice_pick):dice_result_list.pop()
 
+    message=str(dice_num)+'D'+str(dice_face)
+    if(dice_pick<dice_num):message+'K'+str(dice_pick)
+    message+=' = [ '
     dice_count=0
-    for i in range(len(dice_result_list)):
+    for i in range(dice_pick):
         if i:message+=' + '
         message+=str(dice_result_list[i])
         dice_count+=dice_result_list[i]
