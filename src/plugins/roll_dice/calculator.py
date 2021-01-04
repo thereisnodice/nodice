15becc4f820298a7ba6d1311f2d0754cd0e3cf45
Jigsaw <j1g5aw@foxmail.com>
2021-01-04 19:00:12 +0800

    Add is_show_detail option  for roll_dice

--- src/plugins/roll_dice/calculator.py
+++ src/plugins/roll_dice/calculator.py
@@ -4,7 +4,7 @@ import re
 # 替换标准骰子及惩罚奖励骰(3d6, 4d6k3)
 def dice_caculator(dice_expression):
 
-    
+    is_show_detail=False
 
     # 匹配标准掷骰
     match_result=re.search(r"([0-9]*)d([0-9]*)(k([0-9]*))?",dice_expression)
@@ -26,14 +26,17 @@ def dice_caculator(dice_expression):
         dice_result_list.append(dice_result)
 
     message=str(dice_num)+'D'+str(dice_face)
-    if(dice_pick<dice_num):message+'K'+str(dice_pick)
-    message+=' = [ '
+    if(dice_pick<dice_num):message+'K'+str(dice_pick)+' = '
+    if is_show_detail:message+='[ '
     dice_count=0
     for i in range(dice_pick):
-        if i:message+=' + '
-        message+=str(dice_result_list[i])
+        if is_show_detail:
+            if i:message+=' + '
+            message+=str(dice_result_list[i])
         dice_count+=dice_result_list[i]
-    message+=' ] = '+str(dice_count)
+    if is_show_detail:
+        message+=' ]'
+    message+=' = '+str(dice_count)
     count=0
 
     return message.upper()
