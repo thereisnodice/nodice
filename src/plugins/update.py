a4ded8ed2b25f0294679cd18e0480b446e801e83
Jigsaw <j1g5aw@foxmail.com>
2021-01-05 19:05:46 +0800

    Update update.py (add version check)

--- src/plugins/update.py
+++ src/plugins/update.py
@@ -1,6 +1,7 @@
 import subprocess
 from subprocess import PIPE, STDOUT, CalledProcessError
 from nonebot import CommandSession, on_command, permission
+from __version__ import *
 
 __plugin_name__ = '[E][A]update'
 __plugin_usage__ = r"""
@@ -21,6 +22,7 @@ async def update(session: CommandSession):
         output = subprocess.run(['git', 'pull'], stdout=PIPE, stderr=STDOUT).stdout.decode('utf-8').strip()
         msg = output.replace('https://', '').replace('http://', '') # 防止QQ将链接渲染为卡片
         await session.send(msg.strip())
+        await session.send('成功更新至'+version)
 
     except CalledProcessError as e:
         await session.send(f"Fail to update: {e}")
