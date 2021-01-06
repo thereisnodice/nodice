da4f5a75ecec2fd6b2b43391359f0ff15db1f28a
Jigsaw <j1g5aw@foxmail.com>
2021-01-07 01:37:08 +0800

    Update bot_control (try to fix this bug)

--- src/plugins/bot_control/__init__.py
+++ src/plugins/bot_control/__init__.py
@@ -41,9 +41,9 @@ async def update(session: CommandSession):
     try:
         output = subprocess.run(['git', 'pull'], stdout=PIPE, stderr=STDOUT).stdout.decode('utf-8').strip()
         msg = output.replace('https://', '').replace('http://', '') # 防止QQ将链接渲染为卡片
-        await session.send(msg.strip())
-        await reload(session)
-        await version(session)
+        session.send(msg.strip())
+        reload(session)
+        version(session)
     except CalledProcessError as e:
         await session.send(f"Fail to update: {e}")
 
