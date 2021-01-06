e38b584b608ac6c329bb24a65383ee37e8110c6f
Jigsaw <j1g5aw@foxmail.com>
2021-01-06 13:19:46 +0800

    Fix bot.reload'bug (reload counld not reload deleted plugins)

--- src/plugins/bot/__init__.py
+++ src/plugins/bot/__init__.py
@@ -1,10 +1,10 @@
-from nonebot import on_command, CommandSession, permission
+from nonebot import on_command, CommandSession, permission,get_loaded_plugins
 from nonebot.plugin import reload_plugin,load_plugin
 import subprocess
 from subprocess import PIPE, STDOUT, CalledProcessError
 import os
 
-VERSION='0.10.12_α'
+VERSION='0.10.13_α'
 
 __plugin_name__ = 'bot'
 __plugin_usage__ = """
@@ -21,14 +21,19 @@ async def bot(session: CommandSession):
     else:await version(session)
 
 async def reload(session: CommandSession):
+    plugin_list=set()
+    for p in get_loaded_plugins():
+        plugin_list.add(p.module.__name__)
     for f in os.listdir('./plugins/'):
-        if f != '__pycache__' :
+        if f!='__pycache__':
             plugin_path = 'plugins.' + os.path.splitext(f)[0]
-            ret = reload_plugin(plugin_path)
-            if not ret:
-                ret = load_plugin(plugin_path)
-            if not ret:
-                await session.send("[WARNING] Failed to reload '%s' plugin!" %plugin_path)
+            plugin_list.add(plugin_path)
+    for plugin_path in plugin_list:
+        ret = reload_plugin(plugin_path)
+        if not ret:
+            ret = load_plugin(plugin_path)
+        if not ret:
+            await session.send("[WARNING] Failed to reload '%s' plugin!" %plugin_path)
     await session.send("Plugins reloaded.")
 
 async def update(session: CommandSession):
@@ -43,4 +48,4 @@ async def update(session: CommandSession):
         await session.send(f"Fail to update: {e}")
 
 async def version(session: CommandSession):
-    await session.send('NoDice by Jigsaw Version '+VERSION+'\n')
+    await session.send('NoDice by Jigsaw Version '+VERSION)
--- src/plugins/usage.py
+++ src/plugins/usage.py
@@ -1,12 +1,9 @@
-import nonebot
-
-from nonebot import on_command, CommandSession
-
+from nonebot import on_command, CommandSession,get_loaded_plugins
 
 @on_command('usage', aliases=['help','使用帮助', '帮助', '使用方法'],only_to_me=False)
 async def _(session: CommandSession):
     # 获取设置了名称的插件列表
-    plugins = list(filter(lambda p: p.name, nonebot.get_loaded_plugins()))
+    plugins = list(filter(lambda p: p.name,get_loaded_plugins()))
 
     arg = session.current_arg_text.strip().lower()
     if not arg:
