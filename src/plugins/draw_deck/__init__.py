a115d9604035c150abfb0965fa29bda52a5c41a9
Jigsaw <j1g5aw@foxmail.com>
2021-01-14 13:55:45 +0800

    Update draw_deck plugin (.ti/li & .name)

--- src/plugins/draw_deck/__init__.py
+++ src/plugins/draw_deck/__init__.py
@@ -1,4 +1,5 @@
 from nonebot import on_command, CommandSession
+from nonebot.command import call_command
 from .decks import get_deck_list,separate_deckname_and_num
 
 __plugin_name__ = '拓展牌堆'
@@ -20,3 +21,18 @@ async def ndraw(session: CommandSession):
     if args=='list':await session.send(get_deck_list())
     else:
         await session.send(separate_deckname_and_num(args,True))
+
+@on_command('name',only_to_me=False)
+async def name(session:CommandSession):
+    try:
+        await call_command(session.bot,session.event,'draw',current_arg='_name '+str(int(session.current_arg_text.strip())))
+    except:
+        await call_command(session.bot,session.event,'draw',current_arg='_name_'+session.current_arg_text.strip())
+
+@on_command('ti',only_to_me=False)
+async def ti(session:CommandSession):
+    await call_command(session.bot,session.event,'draw',current_arg='_即时症状')
+
+@on_command('li',only_to_me=False)
+async def li(session:CommandSession):
+    await call_command(session.bot,session.event,'draw',current_arg='_总结症状')
