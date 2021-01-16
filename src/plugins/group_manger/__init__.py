93c9e9cffe7872b5b9510070c87ab6bc2d58bda9
Jigsaw <j1g5aw@foxmail.com>
2021-01-16 22:48:13 +0800

    Update group_manger (add easy .dismiss)

--- src/plugins/group_manger/__init__.py
+++ src/plugins/group_manger/__init__.py
@@ -1,6 +1,15 @@
-from nonebot import on_request, RequestSession
+from nonebot import on_request,RequestSession,on_command,CommandSession
+from nonebot.permission import GROUP_ADMIN,SUPERUSER
 
 # 将函数注册为群请求处理器
 @on_request('group')
 async def _(session: RequestSession):
         await session.approve()
+
+@on_command('dismiss',permission=GROUP_ADMIN|SUPERUSER)
+async def dismiss(session: CommandSession):
+    await session.bot.set_group_leave(group_id=session.event.group_id)
+
+@on_command('bot',only_to_me=False)
+async def bot(session: CommandSession):
+        pass
