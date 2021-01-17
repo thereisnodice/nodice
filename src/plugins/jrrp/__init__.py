5cfe44c5166f9bc8d8c78f10caba70d7788984b1
Jigsaw <j1g5aw@foxmail.com>
2021-01-17 21:31:59 +0800

    Update jrrp plugin (add online jrrp option)

--- src/plugins/jrrp/__init__.py
+++ src/plugins/jrrp/__init__.py
@@ -1,8 +1,5 @@
 from nonebot import on_command, CommandSession
-import json
-import time 
-import os
-import random
+from .data_source import get_jrrp
 
 __plugin_name__ = '今日人品'
 __plugin_usage__ = (
@@ -12,22 +9,6 @@ __plugin_usage__ = (
 
 @on_command('jrrp', aliases=('今日人品'),only_to_me=False)
 async def jrrp(session: CommandSession):
-    day=str(time.localtime(time.time())[2])
+    bot_qq_id=str(session.event.self_id)
     qq_id=str(session.event.user_id)
-    try:
-        jrrp_data=open(os.path.join(os.path.dirname(__file__),'jrrp_data.json'),'r',encoding='utf-8')
-        jrrp_data_json=json.loads(jrrp_data.read())
-        jrrp_data.close()
-    except:
-        jrrp_data_json={}
-    if qq_id in jrrp_data_json.keys() :
-        if day!=jrrp_data_json[qq_id]['day']:
-            jrrp=str(random.randint(1,100))
-            jrrp_data_json[qq_id]={'day':day,'jrrp':jrrp}
-    else:
-        jrrp=str(random.randint(1,100))
-        jrrp_data_json[qq_id]={'day':day,'jrrp':jrrp}
-    jrrp_data=open(os.path.join(os.path.dirname(__file__),'jrrp_data.json'),'w',encoding='utf-8')
-    jrrp_data.write(json.dumps(jrrp_data_json))
-    jrrp_data.close()
-    await session.send('你今天的人品值是：'+jrrp_data_json[qq_id]['jrrp'])
+    await session.send(get_jrrp(bot_qq_id,qq_id))
---
+++ src/plugins/jrrp/data_source.py
@@ -0,0 +1,44 @@
+import json
+import time 
+import os
+import random
+import requests
+
+# 是否使用溯洄的API以达到与Dice!同步的目的
+IS_ONLINE=False
+
+def get_jrrp_local(qq_id):
+    day=str(time.localtime(time.time())[2])
+    try:
+        jrrp_data=open(os.path.join(os.path.dirname(__file__),'jrrp_data.json'),'r',encoding='utf-8')
+        jrrp_data_json=json.loads(jrrp_data.read())
+        jrrp_data.close()
+    except:
+        jrrp_data_json={}
+    if qq_id in jrrp_data_json.keys() :
+        if day!=jrrp_data_json[qq_id]['day']:
+            jrrp=str(random.randint(1,100))
+            jrrp_data_json[qq_id]={'day':day,'jrrp':jrrp}
+    else:
+        jrrp=str(random.randint(1,100))
+        jrrp_data_json[qq_id]={'day':day,'jrrp':jrrp}
+    jrrp_data=open(os.path.join(os.path.dirname(__file__),'jrrp_data.json'),'w',encoding='utf-8')
+    jrrp_data.write(json.dumps(jrrp_data_json))
+    jrrp_data.close()
+
+    return '你今天的人品值是：'+jrrp_data_json[qq_id]['jrrp']
+
+def get_jrrp_online(bot_qq_id,qq_id):
+    url='http://api.kokona.tech:5555/jrrp'
+    data={'User-Agent':'NoDice','QQ':bot_qq_id,'v':'20190114','QueryQQ':qq_id}
+    res=requests.post(url=url,data=data)
+    return '你今天的人品值是：'+res.text
+
+def get_jrrp(bot_qq_id,qq_id):
+    if IS_ONLINE:
+        return get_jrrp_online(bot_qq_id,qq_id)
+    else:
+        return get_jrrp_local(qq_id)
+
+if __name__=='__main__':
+    print(get_jrrp_online()
